9c9a9df2a9b1cbcfd2d4ce0955ec93238b787e3e
Travis Mix <travismix@Traviss-MacBook-Pro.local>
2014-02-05 00:41:49 -0800

    cleared the repo
    cleared the repo

--- IndieGamesList/Resources/app.js
+++
@@ -1,9 +0,0 @@
-//Indie Games List 
-//MIU1401
-//by Travis Mix
-//January 20, 2014 
-
-
-//include our other code files into the project
-Ti.include("MainScreen.js");
-//this is all that is needed here in app.js to make it work a very simple main(entry point) for the app :)
