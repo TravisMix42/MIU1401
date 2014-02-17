b093a45187319d746a93abfe0799ec67199aa93e
Travis Mix <travismix@Traviss-MacBook-Pro.local>
2014-02-17 14:40:40 -0800

    addWin added
    added a new window to the app that will be used to add data to the
    database

---
+++ Cruddy/Resources/add.js
@@ -0,0 +1,12 @@
+//add.js code is for the add view of the cruddy application
+//this is the view that will allow the user to add new information 
+//to the database...
+//by Travis A. Mix
+//on Febuary 17, 2014
+//for asdi1402 week 3
+
+//create our add window
+var addWin = Ti.UI.createWindow({
+	title : "Add Data",
+	backgroundColor : "#707070"
+});
