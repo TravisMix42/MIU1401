7a556c4eb11bfd2be0d007d14c77334aef812762
Travis Mix <travismix@Traviss-MacBook-Pro.local>
2014-02-23 11:13:03 -0800

    App created
    App created and login screen done uses a preset password only may add
    ability to create a new user account if I have time before turning in
    the assignment

---
+++ hungry/Resources/main.js
@@ -0,0 +1,10 @@
+//main.js for hungry app 
+//by Travis Mix
+//on Febuary 23, 2014
+//for Doug Arley asdi 1402
+
+//create our main app window
+var appMain = Ti.UI.createWindow({
+	title : "main",
+	backgroundColor : "#909090"
+});
